a1c0bb91380f518a46dc30301916d930ef6a31f1
Carsten Kragelund <carsten@kragelund.dev>
2022-10-15 21:39:59 +0200

    Add more list functions

--- list.h
+++ list.h
@@ -17,9 +17,12 @@
     errno = 26; \
     r; \
 }
-#define new_list(T) LIST_FN(new, T)
 #define push_list(T) LIST_FN(push, T)
 #define get_list(T) LIST_FN(get, T)
+#define dedup_list(T) LIST_FN(dedup, T)
+#define swap_list(T) LIST_FN(swap, T)
+#define remove_list(T) LIST_FN(remove, T)
+#define with_capacity_list(T) LIST_FN(with_capacity, T)
 
 typedef struct {
   void* data;
@@ -37,7 +40,7 @@ void free_list(List* self) {
 long length_list(List* self) {
   return self->length;
 }
-long capcity_list(List* self) {
+long capacity_list(List* self) {
   return self->capacity;
 }
 void clear_list(List* self) {
@@ -52,20 +55,30 @@ int is_free_list(List* self) {
 int is_empty_list(List* self) {
   return self->length == 0;
 }
-
-#endif
-List LIST_FN(new, LIST_TYPE) ();
-void LIST_FN(push, LIST_TYPE) (List* self, LIST_TYPE data);
-LIST_TYPE LIST_FN(get, LIST_TYPE) (List* self, long index);
-void LIST_FN(dedup, LIST_TYPE) (List* self, int (*compare)(LIST_TYPE, LIST_TYPE));
-void LIST_FN(shrink,LIST_TYPE) (List* self, long count);
-void LIST_FN(swap,LIST_TYPE) (List* self, long first_index, long second_index);
-
-List LIST_FN(new, LIST_TYPE) () {
+void shrink_list (List* self, long count) {
+  self->length -= count;
+}
+List new_list() {
   return (List){
     .data = (void*)0,
     .length = 0,
     .capacity = 0,
+  };
+}
+
+#endif
+void LIST_FN(push,LIST_TYPE) (List* self, LIST_TYPE data);
+LIST_TYPE LIST_FN(get,LIST_TYPE) (List* self, long index);
+void LIST_FN(dedup, LIST_TYPE) (List* self, int (*compare)(LIST_TYPE, LIST_TYPE));
+void LIST_FN(swap,LIST_TYPE) (List* self, long first_index, long second_index);
+void LIST_FN(remove,LIST_TYPE) (List* self, long index);
+List LIST_FN(with_capacity,LIST_TYPE) (long capacity);
+
+List LIST_FN(with_capacity,LIST_TYPE) (long capacity) {
+  return (List){
+    .data = malloc(sizeof(LIST_TYPE) * capacity),
+    .length = 0,
+    .capacity = capacity,
   };
 }
 
@@ -121,14 +134,30 @@ void LIST_FN(dedup, LIST_TYPE) (List* self, int (*compare)(LIST_TYPE, LIST_TYPE)
       }
     }
   }
-  LIST_FN(shrink, LIST_TYPE)(self, count);
+  shrink_list(self, count);
 }
 
-void LIST_FN(shrink,LIST_TYPE) (List* self, long count) {
-  self->length -= count;
-}
 
 void LIST_FN(swap,LIST_TYPE) (List* self, long first_index, long second_index) {
   ASSERT_NOT_FREE(return)
   memswp(self->data + sizeof(LIST_TYPE) * first_index, self->data + sizeof(LIST_TYPE) * second_index, sizeof(LIST_TYPE));
+}
+
+void LIST_FN(remove,LIST_TYPE) (List* self, long index) {
+  ASSERT_NOT_FREE(return)
+  for (long i = index; i < self->length; i++) {
+    *(LIST_TYPE*)(self->data + sizeof(LIST_TYPE) * i) = *(LIST_TYPE*)(self->data + sizeof(LIST_TYPE) * (i + 1));
+  }
+  shrink_list(self, 1);
+}
+
+List LIST_FN(filter,LIST_TYPE) (List* self, int (*pred)(LIST_TYPE*)) {
+  ASSERT_NOT_FREE(volatile List* ptr = 0; return *ptr);
+  List new = with_capacity_list(LIST_TYPE)(self->capacity);
+  for (int i = 0; i < self->length; i++) {
+    if (pred((LIST_TYPE*)(self->data + sizeof(LIST_TYPE) * i))) {
+      LIST_FN(push,LIST_TYPE)(&new, LIST_FN(get, LIST_TYPE)(self, i));
+    }
+  }
+  return new;
 }
